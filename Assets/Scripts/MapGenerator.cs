4f9553f43aff74c9caa079c5c451ad99c73fc518
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2022-10-13 17:04:31 -0300

    Marching Squares

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -37,6 +37,8 @@ public class MapGenerator : MonoBehaviour
         {
             SmoothMap();
         }
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(map, 1);
    }
 
     void RandomFillMap() // works through the double nested loop and picks a random state (wall / open)
@@ -108,17 +110,17 @@ public class MapGenerator : MonoBehaviour
 
     private void OnDrawGizmos() // TEMP map draw
     {
-        if (map != null)
+/*        if (map != null)
         {
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    Gizmos.color = (map[x,y] == 0) ? Color.white : Color.black;
+                    Gizmos.color = (map[x,y] == 0) ? Color.green : Color.blue;
                     Vector3 pos = new Vector3(-width / 2 + x + 0.5f, 0, -height / 2 + y + 0.5f);
                     Gizmos.DrawCube(pos, Vector3.one);
                 }
             }
-        }
+        }*/
     }
 }
