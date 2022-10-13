e51cb6c2885f78df32092c322bcba02cb34bcd7a
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2022-10-13 16:25:31 -0300

    Temp cave Gen (Cellular Automata)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -16,12 +16,20 @@ public class MapGenerator : MonoBehaviour
 
     int[,] map;
 
-    private void Start()
+    void Start()
     {
         GenerateMap();
     }
 
-   void GenerateMap()
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
    {
         map = new int[width, height];
         RandomFillMap();
@@ -31,11 +39,11 @@ public class MapGenerator : MonoBehaviour
         }
    }
 
-    void RandomFillMap()
+    void RandomFillMap() // works through the double nested loop and picks a random state (wall / open)
     {
         if (useRandomSeed)
         {
-            seed = Time.time.ToString();
+            seed = System.DateTime.Now.ToString(); // Time.time wouldn't work for me so I am using the computers time instead
         }
 
         System.Random PseudoRandom = new System.Random(seed.GetHashCode()); // sets seed to int
@@ -56,32 +64,49 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-    void SmoothMap()
+    void SmoothMap() // if there are four walls next to the current tile make the current tile a wall, otherwise make it open
     {
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
+                int neighborWallTiles = GetSurroundingWallCount(x, y);
 
+                if (neighborWallTiles > 4)
+                {
+                    map[x, y] = 1;
+                }
+                else if (neighborWallTiles < 4)
+                {
+                    map[x, y] = 0;
+                }
             }
         }
     }
 
 
-    void GetSurroundingWallCount(int x, int y)
+    int GetSurroundingWallCount(int gridX, int gridY) // Counts the surrounding walls (they are listed 0 or 1) and adds the number for SmoothMap()
     {
         int wallCount = 0;
-        for (int neighborX = -1; neighborX <= x + 1; neighborX++)
+        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
         {
-            for (int neighborY = -1; neighborY <= y + 1; neighborY++)
+            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
             {
-
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+                {
+                    if (neighborX != gridX || neighborY != gridY)
+                    {
+                        wallCount += map[neighborX, neighborY];
+                    }
+                }
+                else wallCount++;
             }
         }
+        return wallCount;
     }
     
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmos() // TEMP map draw
     {
         if (map != null)
         {
